a208403f307dc6cbd19900b62b78badc0d41f4e1
Bharadwaj <simhadriravi21@gmail.com>
2026-02-04 11:06:00 +0530

    Add files via upload

---
+++ string related operation.py
@@ -0,0 +1,13 @@
+s1 = "Hello"
+s2 = 'Python'
+s3 = """Welcome to string demo"""
+s4 = s1 + " " + s2
+print("Concatenation (s1 + ' ' + s2):", s4)
+
+print("Repetition (s1 * 3):", s1*3)
+sample = "   hello python world   "
+print("Original sample: '", sample, "'", sep="")
+print("upper()       ->", sample.upper())
+print("lower()       ->", sample.lower())
+words = sample.split()
+print("split() ->", words)
